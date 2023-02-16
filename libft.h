0cf38432b8d0cfa8a8facd5fe8c243d37f59c9e8
Olskor <105324070+Olskor@users.noreply.github.com>
2023-02-16 13:35:00 +0100

    Add files via upload

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jauffret <jauffret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:36:28 by jauffret          #+#    #+#             */
-/*   Updated: 2023/02/06 11:16:17 by jauffret         ###   ########.fr       */
+/*   Updated: 2023/02/06 18:45:05 by jauffret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,6 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
-//bonus
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
